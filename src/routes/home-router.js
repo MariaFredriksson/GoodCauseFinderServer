24b2bc6990eb9920e62b6f14596cdb0626c24b97
Maria Fredriksson <mf223wk@student.lnu.se>
2023-04-27 08:41:35 +0200

    Make all but one router and controller unavailable

--- src/routes/home-router.js
+++ src/routes/home-router.js
@@ -15,6 +15,6 @@ const controller = new HomeController()
 
 // Defines a route that will handle GET responses
 // / is the first argument passed to get(), and it's the root path
-// (req, res, next) is the secon argument passed to get(), and it's a callback function that will execute when a GET request is made to the root
+// (req, res, next) is the second argument passed to get(), and it's a callback function that will execute when a GET request is made to the root
 // The last part of the callback function calls on the index method of the controller and sends 3 arguments. The controller will then handle all the logic of the home site
 router.get('/', (req, res, next) => controller.index(req, res, next))
--- src/routes/router.js
+++ src/routes/router.js
@@ -7,20 +7,22 @@
 
 import express from 'express'
 import { router as homeRouter } from './home-router.js'
-import { router as snippetsRouter } from './snippets-router.js'
-import { router as usersRouter } from './users-router.js'
+// import { router as snippetsRouter } from './snippets-router.js'
+// import { router as usersRouter } from './users-router.js'
 
 export const router = express.Router()
 
 router.use('/', homeRouter)
-router.use('/snippets', snippetsRouter)
-router.use('/users', usersRouter)
+// router.use('/snippets', snippetsRouter)
+// router.use('/users', usersRouter)
 
 // If any url is requested that doesn't match the above, there will be an error
 router.use('*', (req, res, next) => {
   const error = new Error('Not Found')
-  error.status = 404
 
-  // Sends the errror to the the next middleware, which will trigger the error handler
+  // TODO: set another status code
+  // error.status = 404
+
+  // Sends the error to the the next middleware, which will trigger the error handler
   next(error)
 })
