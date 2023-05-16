f3d684bb65702785d902da14e3867ec098148499
Maria Fredriksson <mf223wk@student.lnu.se>
2023-05-16 17:11:47 +0200

    Delete some old code

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,7 @@
-// const config = {
-//   testEnvironment: 'node'
-// }
-
-// import { transform } from "@babel/core";
-
-// // module.exports = config
-// export default config
-
-// export default {
-//   testEnvironment: 'jest-environment-node',
-//   transform: {}
-// }
-
-// {
-//   "transform": {
-//     "^.+\\.js$": "babel-jest"
-//   }
-// }
-
-/*
+/**
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-
 export default {
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
@@ -31,9 +10,4 @@ export default {
   testEnvironment: 'node',
 
   transform: {}
-
-  // setupFilesAfterEnv: [
-  //   'jest-extended/all',
-  //   'jest-chain'
-  // ]
 }
