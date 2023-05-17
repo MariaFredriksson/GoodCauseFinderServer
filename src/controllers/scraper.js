e42dd22bacf1cd27bdb7b64d591014db711ec837
Maria Fredriksson <mf223wk@student.lnu.se>
2023-05-17 09:41:39 +0200

    Delete old code

--- src/controllers/scraper.js
+++ src/controllers/scraper.js
@@ -1,6 +1,4 @@
 
-import { JSDOM } from 'jsdom'
-
 // Import puppeteer for scraping dynamic websites.
 import puppeteer from 'puppeteer'
 
@@ -8,41 +6,6 @@ import puppeteer from 'puppeteer'
  * A class that represents a general scraper for extracting elements from a webpage.
  */
 export class Scraper {
-  /**
-   * Extracts elements from a webpage.
-   *
-   * @param {string} url - The URL of the webpage to scrape.
-   * @param {string} elementName - The name of the element to extract (e.g. 'a', 'div').
-   * @param {object} ifAnyOptions - An optional options object that can be passed to the fetch function.
-   *
-   * @returns {Promise} A promise of an array of HTML elements.
-   */
-  async extractElements (url, elementName, ifAnyOptions) {
-    const htmlText = await this.getHtmlText(url, ifAnyOptions)
-
-    const dom = new JSDOM(htmlText)
-
-    return Array.from(dom.window.document.querySelectorAll(elementName))
-  }
-
-  /**
-   * Retrieves the HTML text of a webpage.
-   *
-   * @param {string} url - The URL of the webpage to scrape.
-   * @param {object} ifAnyOptions - An optional options object that can be passed to the fetch function.
-   *
-   * @returns {Promise} A promise of the HTML text of the webpage.
-   */
-  async getHtmlText (url, ifAnyOptions) {
-    const response = await fetch(url, ifAnyOptions)
-
-    if (!response.ok) {
-      throw new Error(`Could not fetch. Status: ${response.status}`)
-    }
-
-    return response.text()
-  }
-
   /**
    * Scrapes a webpage using Puppeteer.
    *
@@ -57,31 +20,6 @@ export class Scraper {
 
     // Navigate to the page of the url.
     await page.goto(url)
-
-    // Get the header from its XPath.
-    // Destructure the array to pull out the first element.
-    // const [header] = await page.$x('//*[@id="post-36680"]/header/h1')
-
-    // const text = await page.evaluate(() => document.body.innerText)
-
-    // const article = await page.evaluate(() =>
-    //   Array.from(document.querySelectorAll('#content .main-content'), (e) => ({
-    //     title: e.querySelector('h1').innerText,
-    //     imgURL: e.querySelector('.featured_image img').src,
-    //     text: e.querySelector('.entry-content').innerText
-    //   })
-    //   )
-    // )
-
-    // // Copy the URL
-    // const urlCopy = url
-    // // Get the id from the url
-    // const urlId = urlCopy.split('/').pop()
-
-    // // Split the URL by "/"
-    // const parts = url.split("/")
-    // // Get the second-to-last part of the URL
-    // const urlId = parts[parts.length - 2]
 
     // Get the article by getting the title, image URL and text from the page.
     // Pass url as an argument
